223fa438c3a9a14e9e3bede871a7edf58909c42a
Ken Collins <ken@metaskills.net>
2008-09-22 10:56:56 -0400

    Making good progress on using new SelfGrouping proxy class. Added a few tests that cover the bases. Now on to enumerable.

--- lib/grouped_scope/self_grouping.rb
+++ lib/grouped_scope/self_grouping.rb
@@ -1,12 +1,16 @@
 module GroupedScope
   class SelfGroupping
     
-    include Enumerable
+    attr_reader :proxy_owner
     
-    delegate :size, :first, :last, :[], :inspect, :to => :group
     delegate :primary_key, :quote_value, :columns_hash, :to => :proxy_class
     
-    attr_reader :proxy_owner
+    [].methods.each do |m|
+      unless m =~ /(^__|^nil\?|^send|^object_id$|class|extend|^find$|count|sum|average|maximum|minimum|paginate|first|last|empty\?|respond_to\?)/
+        delegate m, :to => :group_proxy
+      end
+    end
+    
     
     def initialize(proxy_owner)
       raise NoGroupIdError.new(proxy_owner) unless proxy_owner.class.column_names.include?('group_id')
@@ -21,14 +25,6 @@ module GroupedScope
       ids.map { |id| quote_value(id,columns_hash[primary_key]) }.join(',')
     end
     
-    def group
-      @group ||= find_selves(group_scope_options)
-    end
-    
-    def each
-      group.each { |member| yield member }
-    end
-    
     def respond_to?(method, include_private=false)
       super || !proxy_class.grouped_scopes[method].blank?
     end
@@ -36,12 +32,15 @@ module GroupedScope
     
     protected
     
+    def group_proxy
+      @group_proxy ||= find_selves(group_scope_options)
+    end
+    
     def all_grouped?
       false
     end
     
     def no_group?
-      # TODO [group_id] Abstractraction point.
       proxy_owner.group_id.blank?
     end
     
--- test/grouped_scope/self_grouping_test.rb
+++ test/grouped_scope/self_grouping_test.rb
@@ -6,8 +6,29 @@ class SelfGrouppingTest < GroupedScope::TestCase
     setup_environment
   end
   
-  should 'be true' do
-    assert true
+  
+  context 'Calling #group' do
+    
+    should 'return an array' do
+      assert_instance_of Array, Factory(:employee).group
+    end
+    
+    context 'with a NIL group' do
+
+      setup do
+        @employee = Factory(:employee)
+      end
+      
+      should 'return an array of one' do
+        assert_equal 1, @employee.group.size
+      end
+      
+      should 'include self in group' do
+        assert @employee.group.include?(@employee)
+      end
+
+    end
+    
   end
   
   
