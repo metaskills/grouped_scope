c10e9a5f429cec994996b03ee4e67a5bac12a394
Ken Collins <ken@metaskills.net>
2012-06-12 21:09:56 -0400

    Be consistent in argument names for 3.2.6.

--- lib/grouped_scope/arish/relation/predicate_builer.rb
+++ lib/grouped_scope/arish/relation/predicate_builer.rb
@@ -9,12 +9,12 @@ module GroupedScope
       end
       
       module ClassMethods
-        def build_from_hash_with_grouped_scope(engine, attributes, default_table, check_column = true)
+        def build_from_hash_with_grouped_scope(engine, attributes, default_table, allow_table_name = true)
           attributes.select{ |column, value| GroupedScope::SelfGroupping === value }.each do |column_value|
             column, value = column_value
             attributes[column] = value.arel_table[column.to_s].in(value.ids_sql)
           end
-          build_from_hash_without_grouped_scope(engine, attributes, default_table, check_column)
+          build_from_hash_without_grouped_scope(engine, attributes, default_table, allow_table_name)
         end
         
       end
